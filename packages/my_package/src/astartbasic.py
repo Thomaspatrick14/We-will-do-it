195174b6a59b3215cc959e654e5b046ca6913ac0
Sarthak <37041521+Sarthakushirke@users.noreply.github.com>
2023-06-18 16:44:35 +0200

    Update astartbasic.py
    Publish and subscriber defined a

--- packages/my_package/src/astartbasic.py
+++ packages/my_package/src/astartbasic.py
@@ -15,33 +15,26 @@ class MyAstarNode(DTROS):
         self.pub = rospy.Publisher('wheels', Twist2DStamped, queue_size=10)  # Replace with your motor command topic
         self.sub = rospy.Subscriber('odom_data', Pose2DStamped, self.callback)  # Replace with your odometry topic
         
-    def callback(self, data):
-        # process the received odometry data
+    def callback(self, data): 
+        # odometry data
+        odom = data.x
+        rospy.loginfo("Subscriber the odometry: %.2f" %odom)
         
-        # extract relevant information from the odometry message
-        # perform your computations or control logic based on the odometry data
-        
-        # example: extract linear velocity
-        linear_velocity = data.v
-        
-        # example: calculate desired motor command based on odometry data
-        motor_command = linear_velocity * 0.1 # just an example calculation, replace with your own control logic
-        
-        # create and publish the motor command
-        command_msg = Twist2DStamped()
-        command_msg.command = motor_command
-        
-        rospy.loginfo("Publishing motor command: %.2f" % command_msg.command)
-        self.pub.publish(command_msg)
-        
-    def run(self):
-        # keep spinning
-        rospy.spin()
+    def run(self): 
+        # publish message every 1 second
+        rate = rospy.Rate(1) # 1Hz
+        while not rospy.is_shutdown():
+            linear_velocity = 0.1
+            rospy.loginfo("Publishing message: '%s'" % linear_velocity)
+            self.pub.publish(linear_velocity)
+            rate.sleep()
 
 if __name__ == '__main__':
     # create the node
     node = MyAstarNode(node_name='my_astar_node')
     # run the node
     node.run()
+    # keep spinning
+    rospy.spin()
 
 
