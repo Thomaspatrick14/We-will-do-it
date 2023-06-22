02d4cc41fb45703c196c403c10dfd9af26f05ba6
Bratjay01 <126517286+Bratjay01@users.noreply.github.com>
2023-06-22 17:47:19 +0200

    Update astartbasic.py

--- packages/my_package/src/astartbasic.py
+++ packages/my_package/src/astartbasic.py
@@ -4,6 +4,11 @@ import rospy
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import Pose2DStamped  # odometry message type (for x,y,theta)
 from duckietown_msgs.msg import Twist2DStamped  # motor command message type ( for v, omega) 
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Header
+import signal
+import sys
+import cv2
 
 class MyAstarNode(DTROS):
 
@@ -12,29 +17,65 @@ class MyAstarNode(DTROS):
         super(MyAstarNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         
         # construct publisher and subscriber
-        self.pub = rospy.Publisher('wheels', Twist2DStamped, queue_size=10)  # Replace with your motor command topic
-        self.sub = rospy.Subscriber('odom_data', Pose2DStamped, self.callback)  # Replace with your odometry topic
+        #self.pub = rospy.Publisher('/db8/velocity', Twist2DStamped, queue_size=10)  # Replace with your motor command topic
+        self.pub = rospy.Publisher('/db8/joy_mapper_node/car_cmd', Twist2DStamped, queue_size=10)
+        self.sub = rospy.Subscriber('/db8/velocity_to_pose_node/pose', Pose2DStamped, self.callback)  # Replace with your odometry topic
+        self.stop_flag = False
         
-    def callback(self, data): 
-        # odometry data
-        odom = data.x
-        rospy.loginfo("Subscriber the odometry: %.2f" %odom)
+    def callback(self, data):
+       x = data.x
+       y = data.y
+       theta = data.theta
+       rospy.loginfo("Odometry: x=%.2f, y=%.2f, theta=%.2f", x, y, theta)
+
         
     def run(self): 
-        # publish message every 1 second
+    #     # publish message every 1 second
         rate = rospy.Rate(1) # 1Hz
-        while not rospy.is_shutdown():
-            linear_velocity = 0.1
-            rospy.loginfo("Publishing message: '%s'" % linear_velocity)
-            self.pub.publish(linear_velocity)
+       
+
+        while not rospy.is_shutdown() and not self.stop_flag:
+            twist = Twist2DStamped()
+            twist.header = Header()
+            twist.header.stamp = rospy.Time.now()
+            twist.header.frame_id = " "
+            twist.v = 0.3  # Set the linear velocity
+            twist.omega = 3 # Set the angular velocity
+            rospy.loginfo("Publishing message: '%s'" % twist)
+            self.pub.publish(twist)
             rate.sleep()
 
+            
+
+    # Check for Ctrl+C signal
+            if cv2.waitKey(1) == 3:  # ASCII code for Ctrl+C is 3
+                self.stop_bot()
+    
+    def stop_bot(self):
+        self.stop_flag = True
+
+        # while not rospy.is_shutdown():
+        #     twist = Twist()
+        #     twist.linear.x = 0.1  # Set the linear velocity
+        #     twist.angular.z = 0.0  # Set the angular velocity
+        #     rospy.loginfo("Publishing twist command: %s" % twist)
+        #     self.pub.publish(twist)
+        #     rate.sleep()
+        
+
+    
 if __name__ == '__main__':
+    
+    node = MyAstarNode(node_name='astartbasic')
+    #signal.signal(signal.SIGINT, node.keyboard_interrupt_handler)
+    
+    try: 
     # create the node
-    node = MyAstarNode(node_name='my_astar_node')
     # run the node
-    node.run()
+       node.run()
+  
     # keep spinning
-    rospy.spin()
-
-
+    #rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+    
